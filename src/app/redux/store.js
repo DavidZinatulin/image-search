077799b90867aba8e03536b3935f8928e5e55c71
DavidZinatulin <david.zinatulin@synergium.eu>
2018-12-18 03:47:42 +0200

    enable redux devtools in production mode

--- src/app/redux/store.js
+++ src/app/redux/store.js
@@ -4,17 +4,15 @@ import { rootReducer } from 'app/redux/reducers';
 import thunk from "redux-thunk";
 
 export function configureStore(initialState) {
-  let middleware = applyMiddleware(thunk);
+    let middleware = applyMiddleware(thunk);
 
-  if (process.env.NODE_ENV !== 'production') {
     middleware = composeWithDevTools(middleware);
-  }
 
-  const store = createStore(
-    rootReducer,
-    initialState,
-    middleware
-  );
+    const store = createStore(
+        rootReducer,
+        initialState,
+        middleware
+    );
 
-  return store;
+    return store;
 }
