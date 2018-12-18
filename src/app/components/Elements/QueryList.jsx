7ef5a59eb88eda3f082312fe5613725f69c30368
DavidZinatulin <david.zinatulin@synergium.eu>
2018-12-18 03:55:32 +0200

    fixes

--- src/app/components/Elements/QueryList.jsx
+++ src/app/components/Elements/QueryList.jsx
@@ -1,9 +1,11 @@
 import * as React from "react";
 import 'app/sass/components/Elements/QueryList.scss';
+import { default as labels } from "assets/dictionaries/globalDictionary.json";
 
 const QueryList = ({ saved, onClick }) => {
     return (
         <div className="query-list">
+            <div>{labels.saved_queries}:</div>
             {saved.map((item, key) => (
                 <div key={key} onClick={() => onClick(item)} className="list-group-item">{item}</div>
             ))}
